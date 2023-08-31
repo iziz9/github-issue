7b245eaa741f1189158097f5ad902ba42d6b4da1
iziz9 <46iz.us2@gmail.com>
2023-08-31 14:35:29 +0900

    Feat: 메인페이지 무한스크롤(진행중)

--- src/MainPage/MainPage.tsx
+++ src/MainPage/MainPage.tsx
@@ -8,50 +8,42 @@ import Loading from '../common/Loading';
 const MainPage = () => {
 	const [issueList, setIssueList] = useState<ResponseIssueDataType[]>([]);
 	const [page, setPage] = useState<number>(1);
-	//더이상 페이지가 없으면 요청을 하지 않도록
-	const observeRef = useRef<Element | null>(null);
 	const [isLoading, setIsLoading] = useState<boolean>(false);
-	const [target, setTarget] = useState(null);
+	const [isLastPage, setIsLastPage] = useState<boolean>(false); //더이상 페이지가 없으면 요청을 하지 않도록
+	const observeRef = useRef<HTMLDivElement>();
 
-	// const observer = new IntersectionObserver(
-	// 	([entry]) => {
-	// 		if (!entry.isIntersecting) return;
-
-	// 		// observer.unobserve(entry.target);
-	// 		setPage((page) => page + 1);
-	// 	},
-	// 	{
-	// 		rootMargin: '50px 0px',
-	// 		threshold: 0.3,
-	// 	},
-	// );
-	// observeRef.current && observer.observe(observeRef.current);
-
-	useEffect(() => {
-		const requestGetIssues = async () => {
-			setIsLoading(true);
-			try {
-				const { data }: { data: ResponseIssueDataType[] } = await getGithubResponse({
-					issues: '/issues',
-					state: 'open',
-					sort: 'comments',
-					page,
-				});
-				page === 1 ? setIssueList(data) : setIssueList((prev) => [...prev, ...data]);
-			} catch (err) {
-				alert(err);
-			} finally {
-				setIsLoading(false);
+	const ElementRef = (node: HTMLDivElement) => {
+		if (observeRef.current) observeRef.current.disconnect();
+		// 최근 observer를 갖기위해 이전 observer disconnect
+		observeRef.current = new IntersectionObserver(([entry]) => {
+			// entries는 아래 설명참조
+			if (entry.isIntersecting) {
+				setPage((prev) => prev + 1);
 			}
-		};
+		});
+		if (node) observeRef.current.observe(node); // 노드가 있으면 observer.current를 observe
+	};
+
+	const requestGetIssues = async () => {
+		setIsLoading(true);
+		try {
+			const { data }: { data: ResponseIssueDataType[] } = await getGithubResponse({
+				issues: '/issues',
+				state: 'open',
+				sort: 'comments',
+				page,
+			});
+			page === 1 ? setIssueList(data) : setIssueList((prev) => [...prev, ...data]);
+			if (data.length < 10) setIsLastPage(true);
+		} catch (err) {
+			alert(err);
+		} finally {
+			setIsLoading(false);
+		}
+	};
+	useEffect(() => {
 		requestGetIssues();
 	}, [page]);
-
-	// useEffect(() => {
-	// 	if (isLoading) {
-	// 		observeRef.current.unobserve()
-	// 	}
-	// }, [isLoading])
 
 	return (
 		<main>
@@ -64,7 +56,7 @@ const MainPage = () => {
 					</li>
 				))}
 			</IssueListContainer>
-			{isLoading ? <Loading /> : <div ref={observeRef}></div>}
+			{isLoading ? <Loading /> : !isLastPage && <div ref={ElementRef}></div>}
 		</main>
 	);
 };
